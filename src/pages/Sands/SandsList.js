a3ee7717958c467ca302eeb17255aa571aaf6dd4
fancyqin <fancarter007@163.com>
2018-04-20 17:31:30 +0800

    update

--- src/pages/Sands/SandsList.js
+++ src/pages/Sands/SandsList.js
@@ -1,9 +1,10 @@
 import React, { Component,PureComponent } from 'react';
-import { View,Text,StyleSheet,FlatList } from 'react-native';
+import { View,Text,StyleSheet,FlatList,TouchableOpacity,Image } from 'react-native';
 import ScrollableTabView,{ScrollableTabBar} from 'react-native-scrollable-tab-view';
+import Icon from 'react-native-vector-icons/Feather';
+import moment from 'moment';
 import Navbar from '../../component/Navbar';
-
-
+import SandsData from '../../../res/data/sands.json';
 
 
 class SandsListTabPage extends PureComponent{
@@ -12,25 +13,77 @@ class SandsListTabPage extends PureComponent{
         
         this.state = {
             dataSource: [],
-            selected: null
+            refreshing: false       
+            
         }
+        
     }
 
     componentDidMount(){
-        
+        this.loadData();
     }
 
     loadData(){
+
+        this.setState({
+            dataSource: SandsData.info.data
+        })
+    }
+
+    renderRowItem(it){
         
+        return (
+            <View style={styles.sandsListItem}>
+                <TouchableOpacity style={styles.sandMain} onPress={this.props.selectItem} >
+                    <View style={styles.sandItemText} onPress={this.props.onSelect}>
+                        <Text style={styles.sandTitle} numberOfLines={2}>{it.title}</Text>
+                        <Text style={styles.sandAuthor}>{it.author}</Text>
+                        <Text style={styles.sandTime}>{moment(it.addTime).format('YYYY-MM-DD')}</Text>
+                    </View>
+                    <Image style={{width:140,height:100}} source={require('../../../res/img/img.jpg')}  />
+                </TouchableOpacity>
+                <View style={styles.sandOtherWrap}>
+                    
+                    <View style={styles.otherLeft}>
+                    
+                        <View style={{flexDirection:'row'}}>
+                            <TouchableOpacity onPress={this.props.thumbUp}>
+                                <Icon style={{marginRight:5}}  name="thumbs-up" size={16} color="#999" />
+                            </TouchableOpacity>
+                            <Text style={{color: '#555'}}>{it.votes.length}</Text>
+                        </View>
+                        <View style={{flexDirection:'row',marginLeft:20}}>
+                            <Icon style={{marginRight:5,marginLeft:20}} name="more-horizontal" size={16} color="#999" />
+                            <Text style={{color: '#555'}}>{it.commentCount > 999 ? 999:it.commentCount}</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity><Icon name="share-2" size={16} color="#999" /></TouchableOpacity>
+                </View>
+            </View> 
+        )
+    }
+
+    onRefresh(){
+        this.setState({
+            refreshing: true
+        })
+
+        setTimeout(()=>{
+            this.setState({
+                refreshing: false
+            })
+        },2000)
     }
     
     render(){
-        console.log(this.props)
         return (
-            <View style={styles.container}>
+            <View style={{flex:1}}>
                 <FlatList
-                data={[{key:'a'},{key:'b'}]}
-                renderItem={({item}) => <Text>{item.key}</Text>}
+                style={styles.sandItemWrap}
+                data={this.state.dataSource}
+                renderItem={({item}) => this.renderRowItem(item)}
+                onRefresh = {this.onRefresh}
+                refreshing = {this.state.refreshing}
                 />
             </View>
         )
@@ -53,7 +106,6 @@ export default class SandsList extends Component{
                 <Navbar title="SANDS" />
                 <ScrollableTabView 
                     tabBarBackgroundColor='white'
-                    tabBarInactiveTextColor="mintcream"
                     tabBarActiveTextColor="#574435"
                     tabBarInactiveTextColor="#999"
                     tabBarUnderlineStyle ={{backgroundColor:'#574435',height:2}}
@@ -81,5 +133,68 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+
+    sandItemWrap:{
+        flex:1,
+        backgroundColor:'#f3f5f7',
+        paddingTop: 10,
+        paddingBottom: 60,
+    },
+    sandsListItem:{
+        backgroundColor:'white',
+        padding:10,
+        marginLeft:10,
+        marginRight: 10,
+        marginBottom: 10,
+        borderRadius:2,
+        //iOS的阴影
+        shadowColor:'#b5b5b5',
+        shadowOffset:{width:3,height:2},
+        shadowOpacity:0.4,
+        shadowRadius:1,
+        //Android的阴影
+        elevation:2
+    },
+    sandMain:{
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+    },
+    sandItemText:{
+        display:'flex',
+        flexWrap: 'wrap',
+        marginRight: 20,
+        alignContent: 'space-between',
+        flexShrink: 1,
+    },
+    sandTitle:{
+        fontSize: 18,
+        display:'flex',
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    sandAuthor:{
+        color: '#888',
+        fontSize: 16,
+        marginBottom: 10
+    },
+    sandTime:{
+        color:'#888',
+        fontSize: 12
+    },
+    sandOtherWrap:{
+        borderTopWidth: 0.5,
+        borderColor: '#ddd',
+        display:'flex',
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        paddingTop: 10,
+        marginTop: 10,
+    },  
+    otherLeft:{
+        display:'flex',
+        flexDirection: 'row',
     }
+    
 });
